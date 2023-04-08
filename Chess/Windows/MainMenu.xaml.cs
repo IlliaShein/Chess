6d3b589a85f49cf5ffdd850d30fc8a65efaccd0f
Siwn <odesskyevrei@gmail.com>
2023-04-09 02:58:18 +0300

    Moved Windows (xaml files) to folder Windows. Created new window MainMenu with 4 buttons. 3 buttons have logic

---
+++ Chess/Windows/MainMenu.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chess.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ScreenSizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameLogic.ScreenSizeButtonLogicProcession(MainMenuWindow, GridMainMenu);
+        }
+
+        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow newWindow = new MainWindow();
+            newWindow.Show();
+            this.Close();
+        }
+    }
+}
